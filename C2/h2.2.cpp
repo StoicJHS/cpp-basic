eb82e269bccdb2c95d0343a5e827ed8641f155f7
StoicJHS <yh.nam.stoic@gmail.com>
2025-09-21 13:06:47 +0900

    C2

---
+++ C2/h2.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    const int a = 1;
+    const double b = 0.1;
+
+    auto sum = a + b;
+
+    std::cout << sum << std::endl;
+
+    return 0;
+
+}
