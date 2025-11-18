dbf4a7299e05ae8b660cc23e43f5a670f6291b32
StoicJHS <yh.nam.stoic@gmail.com>
2025-11-18 22:57:04 +0900

    2025-11-18 22:55

---
+++ cpp-basic-jp-hinata/C6/h6.11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        if (i % 2 == 0)
+            std::cout << i << std::endl;
+    }
+}
